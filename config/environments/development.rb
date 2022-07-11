caef831d8ecbef8bee78d63b31cbd742bf999a1d
ms-kazu <k-kinebuchi@msd-net.co.jp>
2022-07-11 15:51:41 +0900

    first commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
   config.assets.enabled = false
-  config.assets.compile = false
+  config.assets.compile = true
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,3 +3,12 @@
 set -eux
 
 #kill -9 `cat /var/www/racco/tmp/pids/unicorn.pid`
+
+# cd /var/www/racco/scripts
+
+ps=`ps aux | grep unicorn | wc -l`
+
+if [ -e /var/www/racco/tmp/pids/unicorn.pid ] && [ $ps -gt 0 ]; then
+  echo "process"
+  echo $ps
+fi
